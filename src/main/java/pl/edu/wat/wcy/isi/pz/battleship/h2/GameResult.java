3f69070a364a2b297936dc7fc6f0bf03a56581e3
marcin.niedzielski <marcin.niedzielski@student.wat.edu.pl>
2018-02-03 23:43:55 +0100

    second commit

---
+++ src/main/java/pl/edu/wat/wcy/isi/pz/battleship/h2/GameResult.java
@@ -0,0 +1,62 @@
+package pl.edu.wat.wcy.isi.pz.battleship.h2;
+
+import javafx.concurrent.Task;
+import pl.edu.wat.wcy.isi.pz.battleship.common.Handler;
+import pl.edu.wat.wcy.isi.pz.battleship.controllers.root.RootController;
+import pl.edu.wat.wcy.isi.pz.battleship.program.ProgramSettings;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+
+public class GameResult {
+    private final ExecutorService executorService;
+
+    public GameResult() {
+        executorService = RootController.getInstance().getExecutorService();
+    }
+
+    public void save(Boolean isWin) {
+
+        Task<Void> task = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                EntityManagerFactory emf = Persistence.createEntityManagerFactory(ProgramSettings.getInstance().property("database.name"));
+                EntityManager em = emf.createEntityManager();
+                em.getTransaction().begin();
+                Game game = new Game();
+                game.setWin(isWin);
+                em.persist(game);
+                em.getTransaction().commit();
+                em.close();
+                emf.close();
+                return null;
+            }
+        };
+
+        executorService.submit(task);
+    }
+
+
+    public void getGames(Handler<Result> handler) {
+
+        Task<Result> task = new Task<Result>() {
+            @Override
+            protected Result call() throws Exception {
+                EntityManagerFactory emf = Persistence.createEntityManagerFactory(ProgramSettings.getInstance().property("database.name"));
+                EntityManager em = emf.createEntityManager();
+                Result result = new Result();
+                result.setWinnings((Long) em.createQuery("select count (g) from Game g where g.win = true").getSingleResult());
+                result.setLosses((Long) em.createQuery("select count (g) from Game g where g.win = false").getSingleResult());
+                em.close();
+                emf.close();
+                return result;
+            }
+        };
+
+        task.setOnSucceeded(event -> handler.handle(task.getValue()));
+        executorService.submit(task);
+    }
+}
